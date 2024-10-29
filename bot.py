9cd28c3e0c5dc4d5b14ad46fb73d90c970730055
Spencer Hughes <16389711+spencerhughes@users.noreply.github.com>
2024-10-28 22:36:29 -0400

    Import classes and functions from database file

--- bot.py
+++ bot.py
@@ -3,3 +3,6 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from datetime import datetime, timedelta
+
+# Import the database models and session function
+from database import Event, Participant, get_session
